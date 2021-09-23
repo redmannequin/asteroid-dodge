3635d711f18b5ecdde3f1e16933a4b4feec0ff9b
redmannequin <barinas.flavio@gmail.com>
2021-09-23 16:14:18 -0400

    feat: animate box accrose the screen

--- src/game_state.rs
+++ src/game_state.rs
@@ -1,21 +1,57 @@
-use ggez::{event, graphics, Context, GameResult};
+use ggez::{event, graphics, timer, Context, GameResult};
 
-pub struct GameState {}
+const TARGET_FPS: u32 = 60;
+
+pub struct GameState {
+    player: Player,
+}
 
 impl GameState {
     pub fn new() -> Self {
-        GameState {}
+        GameState {
+            player: Player::new(),
+        }
     }
 }
 
 impl event::EventHandler<ggez::GameError> for GameState {
-    fn update(&mut self, _ctx: &mut Context) -> GameResult {
+    fn update(&mut self, ctx: &mut Context) -> GameResult {
+        while timer::check_update_time(ctx, TARGET_FPS) {
+            self.player.update();
+        }
         Ok(())
     }
 
     fn draw(&mut self, ctx: &mut Context) -> GameResult {
         graphics::clear(ctx, [0.0, 0.0, 0.0, 1.0].into());
+        self.player.draw(ctx)?;
         graphics::present(ctx)?;
+        ggez::timer::yield_now();
+        Ok(())
+    }
+}
+
+struct Player {
+    pos: u32,
+}
+
+impl Player {
+    fn new() -> Self {
+        Player { pos: 0 }
+    }
+
+    fn update(&mut self) {
+        self.pos += 1;
+    }
+
+    fn draw(&self, ctx: &mut Context) -> GameResult<()> {
+        let rectangle = graphics::Mesh::new_rectangle(
+            ctx,
+            graphics::DrawMode::fill(),
+            graphics::Rect::new(self.pos as f32, 50.0, 20.0, 20.0),
+            [1.0, 0.5, 0.0, 1.0].into(),
+        )?;
+        graphics::draw(ctx, &rectangle, (ggez::mint::Point2 { x: 0.0, y: 0.0 },))?;
         Ok(())
     }
 }
