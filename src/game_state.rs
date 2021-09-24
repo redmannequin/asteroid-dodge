8c1630ab9c6db9f363a7e838c8193e18a05782ac
redmannequin <barinas.flavio@gmail.com>
2021-09-23 23:31:04 -0400

    feat: mutiple balls bouncing around

--- src/game_state.rs
+++ src/game_state.rs
@@ -1,30 +1,55 @@
-use ggez::{event, graphics, timer, Context, GameResult};
+use ggez::event::KeyCode;
+use ggez::{event, graphics, input, timer, Context, GameResult};
+use mint::Point2;
+use rand::prelude::*;
 
+const SCREEN_HEIGHT: f32 = 600.0;
+const SCREEN_WIDTH: f32 = 800.0;
 const TARGET_FPS: u32 = 60;
 
 pub struct GameState {
-    player: Player,
+    players: Vec<Player>,
 }
 
 impl GameState {
     pub fn new() -> Self {
-        GameState {
-            player: Player::new(),
+        const N: usize = 100;
+        let mut rng = rand::thread_rng();
+        let mut players = Vec::with_capacity(N);
+        for _ in 0..N {
+            players.push(Player::new(
+                rng.gen_range(0.0..SCREEN_WIDTH),
+                rng.gen_range(0.0..SCREEN_HEIGHT),
+                &mut rng,
+            ))
         }
+        GameState { players }
     }
 }
 
 impl event::EventHandler<ggez::GameError> for GameState {
     fn update(&mut self, ctx: &mut Context) -> GameResult {
         while timer::check_update_time(ctx, TARGET_FPS) {
-            self.player.update();
+            if input::keyboard::is_key_pressed(ctx, KeyCode::Up) {
+            } else if input::keyboard::is_key_pressed(ctx, KeyCode::Down) {
+            }
+
+            if input::keyboard::is_key_pressed(ctx, KeyCode::Left) {
+            } else if input::keyboard::is_key_pressed(ctx, KeyCode::Right) {
+            }
+
+            for p in &mut self.players {
+                p.update();
+            }
         }
         Ok(())
     }
 
     fn draw(&mut self, ctx: &mut Context) -> GameResult {
         graphics::clear(ctx, [0.0, 0.0, 0.0, 1.0].into());
-        self.player.draw(ctx)?;
+        for p in &self.players {
+            p.draw(ctx)?;
+        }
         graphics::present(ctx)?;
         ggez::timer::yield_now();
         Ok(())
@@ -32,26 +57,46 @@ impl event::EventHandler<ggez::GameError> for GameState {
 }
 
 struct Player {
-    pos: u32,
+    x_pos: f32,
+    y_pos: f32,
+    x_speed: f32,
+    y_speed: f32,
 }
 
 impl Player {
-    fn new() -> Self {
-        Player { pos: 0 }
+    fn new(x_pos: f32, y_pos: f32, rng: &mut ThreadRng) -> Self {
+        Player {
+            x_pos,
+            y_pos,
+            x_speed: rng.gen_range(-20.0..20.0),
+            y_speed: rng.gen_range(-20.0..20.0),
+        }
     }
 
     fn update(&mut self) {
-        self.pos += 1;
+        if self.x_pos > SCREEN_WIDTH || self.x_pos < 0.0 {
+            self.x_speed *= -1.0;
+        }
+        if self.y_pos > SCREEN_HEIGHT || self.y_pos < 0.0 {
+            self.y_speed *= -1.0;
+        }
+        self.x_pos += self.x_speed;
+        self.y_pos += self.y_speed;
     }
 
     fn draw(&self, ctx: &mut Context) -> GameResult<()> {
-        let rectangle = graphics::Mesh::new_rectangle(
+        let circle = graphics::Mesh::new_circle(
             ctx,
             graphics::DrawMode::fill(),
-            graphics::Rect::new(self.pos as f32, 50.0, 20.0, 20.0),
+            Point2 {
+                x: self.x_pos,
+                y: self.y_pos,
+            },
+            2.0,
+            0.5,
             [1.0, 0.5, 0.0, 1.0].into(),
         )?;
-        graphics::draw(ctx, &rectangle, (ggez::mint::Point2 { x: 0.0, y: 0.0 },))?;
+        graphics::draw(ctx, &circle, (Point2 { x: 0.0, y: 0.0 },))?;
         Ok(())
     }
 }
